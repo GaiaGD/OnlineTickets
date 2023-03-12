3c4fe484a39e1f7e9bb34eab17ac12c8fdd2b5ed
GaiaGD <gaia.digregorio@gmail.com>
2023-03-12 15:11:54 -0400

    js

--- script/script.js
+++ script/script.js
@@ -3,12 +3,13 @@ window.addEventListener('load', resizeFont)
 window.addEventListener('resize', resizeFont)
 
     function resizeFont() {
+        console.log("hi")
         let maxWidth = window.innerWidth
 
         if (maxWidth < 576){
             console.log(maxWidth)
             let textToResizeArray = document.querySelectorAll('.resizedForResponsive')
-            let fontSize = maxWidth / 8
+            let fontSize = maxWidth / 10
             for (i of textToResizeArray){
 
                 i.style.fontSize = fontSize + "px"
