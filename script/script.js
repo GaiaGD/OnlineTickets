3afcd38db5a99d529a85adf67884c11faa4c7127
GaiaGD <gaia.digregorio@gmail.com>
2023-03-15 18:58:15 -0400

    rotating works

--- script/script.js
+++ script/script.js
@@ -24,18 +24,32 @@ function toggleInfo(event){
     toggled.classList.toggle("closed")
 }
 
-
-const textRotating = document.getElementById('text-rotating')
+const textRotatingHero = document.getElementById('text-rotating-hero')
 
 // calculate how many letters to spread evenly
-const charsNumbers = (textRotating.innerHTML.replace(/\s/g, '')).length
+const charsNumbers = (textRotatingHero.innerHTML.replace(/\s/g, '')).length
 const rotation = 360/charsNumbers
-console.log(rotation)
 
-textRotating.innerHTML = textRotating.textContent.replace(/\S/g,"<span>$&</span>");
-console.log(textRotating.innerHTML)
+textRotatingHero.innerHTML = textRotatingHero.textContent.replace(/\S/g,"<span>$&</span>");
+console.log(textRotatingHero.innerHTML)
 
 const ele = document.querySelectorAll('span')
 for (var i = 1; i < ele.length; i++){
     ele[i].style.transform = "rotate("+i*rotation+"deg)"
 }
+
+// MAKE IT MODULAR
+
+// const textRotating = document.getElementById('text-rotating-hero')
+
+// // calculate how many letters to spread evenly
+// const charsNumbers = (textRotating.innerHTML.replace(/\s/g, '')).length
+// const rotation = 360/charsNumbers
+
+// textRotating.innerHTML = textRotating.textContent.replace(/\S/g,"<span>$&</span>");
+// console.log(textRotating.innerHTML)
+
+// const ele = document.querySelectorAll('span')
+// for (var i = 1; i < ele.length; i++){
+//     ele[i].style.transform = "rotate("+i*rotation+"deg)"
+// }
